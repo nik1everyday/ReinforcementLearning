e2db63d3f3c188d84efde2dc085a3c40626315de
Nikita Borisov <nik-everyday@yandex.ru>
2023-08-05 18:01:39 +0300

    Updated `train.py`

--- src/train.py
+++ src/train.py
@@ -44,9 +44,8 @@ def compute_loss(outputs, targets):
 
 
 def train(env):
-    # Set up hyperparameters
-    input_size = 3  # Observation space size of the environment
-    output_size = 1  # Single output value representing the reward
+    input_size = 3
+    output_size = 1
     depth = 2
     num_cells = 32
     learning_rate = 0.001
@@ -54,26 +53,19 @@ def train(env):
     batch_size = 32
     validation_samples = 100
 
-    # Create an instance of the MLP class
     mlp = MLP(in_features=input_size, out_features=output_size, depth=depth, num_cells=num_cells)
 
-    # Define the optimizer
     optimizer = torch.optim.Adam(mlp.parameters(), lr=learning_rate)
 
-    # Prepare the training data
     train_inputs, train_targets = prepare_training_data(env, num_samples=100)
 
-    # Prepare the validation data
     val_inputs, val_targets = prepare_validation_data(env, num_samples=validation_samples)
 
-    # Training loop
     for epoch in range(num_epochs):
-        # Shuffle the training data
         indices = np.random.permutation(len(train_inputs))
         train_inputs = train_inputs[indices]
         train_targets = train_targets[indices]
 
-        # Mini-batch training
         for i in range(0, len(train_inputs), batch_size):
             batch_inputs = train_inputs[i:i + batch_size]
             batch_targets = train_targets[i:i + batch_size]
@@ -82,22 +74,17 @@ def train(env):
             batch_inputs_tensor = torch.tensor(batch_inputs, dtype=torch.float32)
             batch_targets_tensor = torch.tensor(batch_targets, dtype=torch.float32)
 
-            # Forward pass
             outputs = mlp(batch_inputs_tensor)
 
-            # Compute the loss
             loss = compute_loss(outputs, batch_targets_tensor)
 
-            # Backward pass and optimization
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        # Validation
         val_inputs_tensor = torch.tensor(val_inputs, dtype=torch.float32)
         val_targets_tensor = torch.tensor(val_targets, dtype=torch.float32)
         val_outputs = mlp(val_inputs_tensor)
         val_loss = compute_loss(val_outputs, val_targets_tensor)
 
-        # Print the epoch and validation loss
         print(f"Epoch: {epoch+1}/{num_epochs}, Validation Loss: {val_loss.item():.4f}")
