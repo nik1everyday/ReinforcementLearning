bfa12ac3aa163e740337da3d0ad5ebd29f3bd87b
Nikita Borisov <nik-everyday@yandex.ru>
2023-08-05 18:00:33 +0300

    Updated `main.py`

--- main.py
+++ main.py
@@ -3,8 +3,5 @@ from src.environment import EnergyEnvironment
 
 
 if __name__ == "__main__":
-    # Create an instance of the EnergyEnvironment class
     env = EnergyEnvironment(time_period=24, max_energy=1000)
-
-    # Start training
     train(env)
